d94c7528a8e119c602441286767012f662425514
dundermifflin0 <dundermifflinresearch@gmail.com>
2018-12-18 09:52:16 +0000

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+import org.apache.commons.collections.*;
+
+public class Main {
+    public static void main(String args[])
+    {
+        CollectionUtils.get(null, 0);
+        System.out.println("Hello");
+    }
+}
